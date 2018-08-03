d56bbe63f1aa4857cff6a8b0077a0ac8577bc836
LAPTOP-N364A3DK\helmu <helmut.ortmann@t-online.de>
2018-08-03 15:17:16 +0200

    Optimized AddIn Search

---
+++ hoServices/src/AddInSearch/Tv.cs
@@ -0,0 +1,16 @@
+﻿namespace EaServices.AddInSearch
+{
+    /// <summary>
+    /// Tagged Values. It handles 'memo' fields.
+    /// </summary>
+    public class Tv
+    {
+        public string Property;
+        public string Value;
+        public Tv(string property, string value)
+        {
+            Property = property;
+            Value = value;
+        }
+    }
+}
